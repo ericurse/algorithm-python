4fc9c7aa5c9da3065dfa8889dc80ff1059626770
ericurse <siyuanfish@163.com>
2021-02-19 21:47:47 +0800

    1、添加一下文件

---
+++ 数组/__init__.py
@@ -0,0 +1,21 @@
+"""
+__project_ = 'algorithm-python'
+__file_name__ = '__init__.py'
+__author__ = 'Administrator'
+__time__ = '2021/2/19 21:47'
+__product_name = PyCharm
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃        ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
